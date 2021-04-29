30798ab18914bd2d02ae233e596fc5e2fb9ab4b2
HayesAJ83 <72214809+HayesAJ83@users.noreply.github.com>
2021-04-29 23:14:04 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -7,23 +7,23 @@ app = Flask(__name__)
  
 @app.route('/')
 def index(): 
-  return render_template('index_mod.html', title='Main')
+  return render_template('02_index.html', title='Main')
 
-@app.route('/send_email', methods=['POST'])
-def send_email():
-  params = request.get_json()
-  print("params: ")
-  print(params)
-  hostinfo = {
-    'sysname': os.uname()[0],
-    'nodename': os.uname()[1],
-    'release': os.uname()[2],
-    'version': os.uname()[3],
-    'machine': os.uname()[4]
-  }
-  params['server'] = hostinfo
-  params['remote_addr'] = request.remote_addr
-  return jsonify(params)
+#@app.route('/send_email', methods=['POST'])
+#def send_email():
+#  params = request.get_json()
+#  print("params: ")
+#  print(params)
+#  hostinfo = {
+#    'sysname': os.uname()[0],
+#    'nodename': os.uname()[1],
+#    'release': os.uname()[2],
+#    'version': os.uname()[3],
+#    'machine': os.uname()[4]
+#  }
+#  params['server'] = hostinfo
+#  params['remote_addr'] = request.remote_addr
+#  return jsonify(params)
 
 
 if __name__ == "__main__":
