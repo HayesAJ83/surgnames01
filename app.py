ff426ae0019c1ac1e6bb72bbd2a20ebeaaf265fc
HayesAJ83 <72214809+HayesAJ83@users.noreply.github.com>
2021-05-01 12:06:52 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import sys
 import logging
 import os
+import pandas as pd
+import streamlit as st
 from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
  
 @app.route('/')
-def index(): 
+def index():
+  url = 'https://raw.githubusercontent.com/HayesAJ83/SurgicalEps_01/main/Eponyms4python_Lite.csv'
+  df1 = pd.read_csv(url, dtype={'PMID':str,'Year':int})
+  df2 = df1.sort_values(by=['Eponym'],ascending=True)
   return render_template('02_index.html', title='Main')
 
 #@app.route('/send_email', methods=['POST'])
