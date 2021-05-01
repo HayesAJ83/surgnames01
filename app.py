bbe102d97044a8115de295fe193e41b5070487ab
HayesAJ83 <72214809+HayesAJ83@users.noreply.github.com>
2021-05-01 12:10:02 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ app = Flask(__name__)
  
 @app.route('/')
 def index():
-  url = 'https://raw.githubusercontent.com/HayesAJ83/SurgicalEps_01/main/Eponyms4python_Lite.csv'
-  df1 = pd.read_csv(url, dtype={'PMID':str,'Year':int})
-  df2 = df1.sort_values(by=['Eponym'],ascending=True)
+  #url = 'https://raw.githubusercontent.com/HayesAJ83/SurgicalEps_01/main/Eponyms4python_Lite.csv'
+  #df1 = pd.read_csv(url, dtype={'PMID':str,'Year':int})
+  #df2 = df1.sort_values(by=['Eponym'],ascending=True)
   return render_template('02_index.html', title='Main')
 
 #@app.route('/send_email', methods=['POST'])
